91e3508ba092b3829cae0dd1a811e9987b90f679
Edward Sams <edwardsams@email.arizona.edu>
2019-11-01 11:37:03 -0400

    currently working on responses

--- src/app/experts/enter-response/enter-response.component.ts
+++ src/app/experts/enter-response/enter-response.component.ts
@@ -1,12 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Response } from '../../models/Response';
+import { ResponseService } from '../../services/response.service';
 
 @Component({
   selector: 'app-enter-response',
   templateUrl: './enter-response.component.html',
   styleUrls: ['./enter-response.component.css'],
 })
+
 export class EnterResponseComponent implements OnInit {
-  constructor() {}
+  @Output() newResponse: EventEmitter<Response> = new EventEmitter();
+  @Output() updatedResponse: EventEmitter<Response> = new EventEmitter();
+  @Input() response: Response;
+
+  constructor(private responseService: ResponseService) {}
 
   ngOnInit() {}
+
+  addResponse(body) {
+    if (!body) {
+      alert('Please add a Response');
+    } else {
+      this.responseService
+        .saveResponse({ body } as Response)
+        .subscribe(response => {
+          console.log(response);
+          this.newResponse.emit(response);
+        });
+    }
+  }
+
+  updateResponse() {
+    this.responseService
+      .updateResponse(this.response)
+      .subscribe(response => {
+        console.log(response);
+        this.updatedResponse.emit(response);
+      });
+  }
 }
